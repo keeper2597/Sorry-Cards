ce97c9571ca774b6f855518876c8948709ffe719
Blake Cooper <keeper25@gmail.com>
2020-04-13 22:25:25 -0500

    Better images, whole card is the button.

--- Sorry Cards/ContentView.swift
+++ Sorry Cards/ContentView.swift
@@ -17,48 +17,52 @@ struct ContentView: View {
     @State var angle: Double = 0
     
     
+    
     var body: some View {
-        VStack {
-            Spacer(minLength: 25)
-            if showingCard == 0 {
-                Color(.black)
-                    .animation(.easeInOut)
-            } else {
-                Image(deck[showingCard])
-                    .resizable().scaledToFit()
-                    .rotationEffect(.degrees(self.angle))
-                    .imageScale(.large)
-                    .animation(.easeInOut)
-                    
-            }
-            Spacer()
-            Button(action: {
+            
+            VStack {
+                Spacer(minLength: 25)
+                
+                Button(action: {
                     if self.showingCard < self.deck.count - 1 {
                         let counter = self.showingCard
                         self.showingCard = 0
-                        self.angle = -10
-                        self.angle += Double.random(in: 1...20)
-                        do { sleep(1)}
+                        self.angle = -15
+                        self.angle += Double.random(in: 1...30)
+                        
                         self.showingCard = counter + 1
                         self.playCardFlip()
+                        
                     } else {
+                        
                         self.deck.shuffle()
                         self.playShuffleSound()
-                        self.showingCard = 0
+                        self.showingCard = 1
                         
                             
                     }
                 }) {
-                Text("Draw")
-                    .fontWeight(.bold)
-                    .font(.title)
-                    .frame(width: 300, height:100)
-                    .background(Color.red)
-                    .foregroundColor(.black)
-            }
-        }
-        .background(Color.black)
+                    if showingCard == 0 {
+                        Text("Tap to Draw")
+                            .foregroundColor(Color(white:0.8))
+                            .fontWeight(.black)
+                            .font(.largeTitle)
+                            
+                    } else {
+                        
+                        ImageView(showingCard: deck[showingCard], angle: angle)
+
+                        }
+                    }.buttonStyle(PlainButtonStyle())
+                    .transition(.move(edge: .trailing))
+                    .frame(minWidth: 0, idealWidth: .infinity, maxWidth: .infinity, minHeight: 0, idealHeight: .infinity, maxHeight: .infinity, alignment: .center)
+                Spacer(minLength: 25)
+            
+            
         
+        
+        }.background(Color.black)
+                .edgesIgnoringSafeArea(.all)
     }
     
     func playShuffleSound() {
@@ -69,6 +73,26 @@ struct ContentView: View {
         cardFlip.play()
     }
     
+}
+
+struct ImageView : View {
+
+    @State var showAction = false
+    var showingCard: String
+    var angle: Double
+
+    var body: some View {
+        Image(showingCard)
+            .resizable()
+            .scaledToFit()
+            .rotationEffect(.degrees(self.showAction ? 360.0 : 0.0))
+            .animation(self.showAction ? Animation.linear(duration: 0.15) : nil)
+            .rotationEffect(.degrees(angle))
+        .onAppear() {
+            self.showAction = true
+        }
+        
+    }
 }
 
 
